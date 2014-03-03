931a46d911234fbd899312ce8826c01ae082a212
Zack Rock <zacharyrock@hotmail.com>
2014-03-03 00:39:30 -0800

    Imported some stuff. Started random weather manager

---
+++ Assets/WeatherManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeatherManager : MonoBehaviour {
+
+	enum Weather {
+		SNOW, SUN, RAIN, SIZE
+	}
+
+	Weather weather;
+
+	public Material sunSkybox;
+	
+	void Start () {
+		weather = (Weather)Random.Range (0, (int)Weather.SIZE);
+
+		switch (weather) {
+		case Weather.SUN:
+			RenderSettings.skybox = sunSkybox;
+			break;
+		case Weather.RAIN:
+			break;
+		default:
+			print("Weather not implemented");
+			break;
+		}
+	}
+}
